39cec7c965567e75f07e16a59312dcfc92be5f9c
Thalita Costa <thalita.martins@estudante.iftm.edu.br>
2025-12-08 11:33:59 -0300

    Update vite.config.ts
    Adicionando caminho do repositório

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// O nome do repositório é 'Bolos-e-Cia'
+const baseRepoPath = '/Bolos-e-Cia/';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: baseRepoPath, 
+});
