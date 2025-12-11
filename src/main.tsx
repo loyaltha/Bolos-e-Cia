778079c1fb6a39144262760eccbc8aac9adfdf57
Thalita Costa <thalita.martins@estudante.iftm.edu.br>
2025-12-11 16:16:08 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// 1. Importando o roteador
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/Bolos-e-Cia">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
