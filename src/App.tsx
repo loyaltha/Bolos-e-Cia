8003a7dc847027ef1e28c17b579129f58927dfd9
Thalita Costa <thalita.martins@estudante.iftm.edu.br>
2025-12-11 16:21:35 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // Removi o BrowserRouter daqui
 import { CartProvider } from './contexts/CartContext'; 
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
@@ -7,29 +7,28 @@ import { Catalogo } from './pages/Catalogo';
 import { Produto } from './pages/Produto';
 import { Carrinho } from './pages/Carrinho';
 import Contato from './pages/Contato'; 
-import Sobre from './pages/Sobre';     
+import Sobre from './pages/Sobre';      
 import { PoliticaPrivacidade } from './pages/PoliticaPrivacidade';
 
 function App() {
   return (
     <CartProvider>
-      <BrowserRouter>
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/catalogo" element={<Catalogo />} />
-              <Route path="/produto/:id" element={<Produto />} />
-              <Route path="/carrinho" element={<Carrinho />} />
-              <Route path="/sobre" element={<Sobre />} />
-              <Route path="/contato" element={<Contato />} />
-              <Route path="/politica-privacidade" element={<PoliticaPrivacidade />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </BrowserRouter>
+      {/* O BrowserRouter foi removido daqui porque já está no main.tsx */}
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalogo" element={<Catalogo />} />
+            <Route path="/produto/:id" element={<Produto />} />
+            <Route path="/carrinho" element={<Carrinho />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="/politica-privacidade" element={<PoliticaPrivacidade />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </CartProvider>
   );
 }
